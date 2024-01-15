a44d0e8a57205932ed17e675fdc99b5736c7218f
colbyktang <colby.us2012@gmail.com>
2024-01-14 20:35:14 -0600

    Update main app files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import {HashLocationStrategy, LocationStrategy} from '@angular/common';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [provideHttpClient(), provideRouter(routes), provideAnimations(), {provide: LocationStrategy, useClass: HashLocationStrategy}]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
+import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
+import { environment } from '../environments/environment';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: "", component: MainComponent },
-    { path: "login", component: LoginComponent}
+    { path: 'home', component: MainComponent, title: `${environment.title}` },
+    { path: 'login', component: LoginComponent, title: `${environment.title} - Login` },
+    { path: 'dashboard', component: DashboardComponent, title: `${environment.title} - Dashboard`},
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '**', pathMatch: 'full', component: PagenotfoundComponent, title: `${environment.title} Error`}
 ];
