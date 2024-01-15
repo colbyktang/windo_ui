dd7c5ecdd174f8bbc08a9b014035ecb542ac42e5
colbyktang <colby.us2012@gmail.com>
2024-01-15 03:49:29 -0600

    Add dashboard username

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { StorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,6 +9,11 @@ import { NavbarComponent } from '../navbar/navbar.component';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor (private storage: StorageService) {}
+  currentUser : any;
 
+  ngOnInit(): void {
+    this.currentUser = this.storage.getUser().data.user;
+  }
 }
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
+import User from '../models/User';
 
 @Injectable({
   providedIn: 'root'
