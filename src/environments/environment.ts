679b4ead37352e6a78d0dbbd49763980c8756b8f
colbyktang <colby.us2012@gmail.com>
2024-01-14 20:36:33 -0600

    Add environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  title: 'Windo Hub',
+  production: true,
+  withCredentials: true,
+  API_URL: '',
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin':
+      '',
+    Auth: '',
+  },
+};
