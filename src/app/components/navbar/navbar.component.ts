4d030cc58c58dec5a5aec79b59fe16714de36df5
colbyktang <colby.us2012@gmail.com>
2024-01-16 00:16:10 -0600

    Add logout and dashboard button for logged in users

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule} from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { StorageService } from '../../services/storage.service';
+import { AuthService } from '../../services/auth.service';
+import User from '../../models/User';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, RouterLink],
+  imports: [NgIf, MatButtonModule, MatToolbarModule, MatIconModule, MatSidenavModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor (private storageService : StorageService, private authService : AuthService) {
+    this.isLoggedIn = this.storageService.isLoggedIn();
+    if (this.isLoggedIn) {
+      this.user = this.storageService.getUser().data.user;
+    }
+  }
 
+  isLoggedIn : boolean; 
+  user : User = {
+    id: 0,
+    email: '',
+    firstName: '',
+    lastName: '',
+    username: '',
+    role: ''
+  };
+
+  logout () {
+    console.log ("Logged out");
+    this.authService.logout();
+    this.storageService.clean();
+    this.isLoggedIn = this.storageService.isLoggedIn();
+  }
 }
